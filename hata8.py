88250c6e5e651d87a1f87492db3b56d4c3a9dc92
Sistem67 <1awmexpress@gmail.com>
2025-07-10 11:43:01 +0300

    Update and rename hata7.py to hata8.py

--- hata8.py
+++ hata8.py
@@ -9,8 +9,8 @@ from TTS.utils.synthesizer import Synthesizer
 vosk_model_path = "/root/vosk-model-small-tr-0.3"
 
 # Coqui TTS model yolları
-tts_config_path = "/root/abdc/ttsmodels/config.yaml"
-tts_model_path = "/root/abdc/ttsmodels/tr_model.onnx"
+tts_config_path = "/abdc/ttsmodels/config.yaml"
+tts_model_path = "/abdc/ttsmodels/tr_model.onnx"
 
 # VOSK model yükle
 model = Model(vosk_model_path)
@@ -33,7 +33,7 @@ synthesizer = Synthesizer(
 
 # Yayın URL kontrol
 if len(sys.argv) < 2:
-    print("Kullanım: python3 hata7.py <m3u8_link>")
+    print("Kullanım: python3 hata8.py <m3u8_link>")
     sys.exit(1)
 
 url = sys.argv[1]
